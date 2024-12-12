19a25386cc8c3c03534343ec69b113976cc6b273
Javigozu <javigozu5127@gmail.com>
2024-12-12 04:01:18 +0100

    corregidos varios diálogos

--- src/Scenes/DialogScene.js
+++ src/Scenes/DialogScene.js
@@ -77,34 +77,29 @@ export default class MenuScene extends Phaser.Scene {
             this.load.image('inventorcaserocrea-inetors','../assets/fondos/Estudios/estudios-creainators.png');
             this.load.image('buscarconsejo','../assets/fondos/Estudios/estudios-consejo.jpg');
 
+            //Universidad
 
-             //Mantenido
-             this.load.json('dataMantenido','../data/Dialogue/DialogMantenido.json');
-             this.load.json('cartasMantenido','../data/Cartas/CartasMantenido.json');
-            //  this.load.image('backgroundMantenido','../assets/fondos/Mantenido/Mantenido.png');
- 
-             this.load.image('mantenido-genio','../assets/cardImages/Mantenido/GenioIncomprendido.jpeg');
-             this.load.image('mantenido-fan','../assets/cardImages/Mantenido/fan.jpg');
-             this.load.image('mantenido-otro','../assets/cardImages/Mantenido/OtroDiaMas.jpg');
-             this.load.image('mantenido-apoyo','../assets/cardImages/Mantenido/ApoyarEnCasa.png');
-             this.load.image('mantenido-hacuma','../assets/cardImages/Mantenido/Hacuma Mapapa.jpg');
-             this.load.image('mantenido-puf','../assets/cardImages/Mantenido/puffffffff.jpg');
+            this.load.json('dataUni','../../data/Dialogue/DialogUniversidad.json');
+            this.load.json('cartasUni','../data/Cartas/CartasUniversidad.json');
+            this.load.image('backgroundUni','../assets/fondos/Estudios/backgorund-estudios.jpg');
+/*
+            this.load.image('estudios-uni','../assets/cardImages/Uni/estudios-universidad.jpg');
+            this.load.image('estudios-fp','../assets/cardImages/Estudios/estudios-fp.jpg');
+            this.load.image('estudios-chamba','../assets/cardImages/Estudios/estudios-chamba.png');
+            this.load.image('estudios-nini','../assets/cardImages/Estudios/estudios-nini.jpg');
+            this.load.image('estudios-creaInator','../assets/cardImages/Estudios/estudios-creainators.png');
+            this.load.image('estudios-consejo','../assets/cardImages/Estudios/estudios-consejo.jpg');
 
-            // Bar
-            this.load.json('dataBar','../data/Dialogue/DialogBar.json');
-            this.load.json('cartasBar','../data/Cartas/CartasBar.json');
-            this.load.image('backgroundBar','../assets/fondos/Bar/BarBG.jpg');
-
-            this.load.image('bar-ayuda','../assets/cardImages/Bar/Llorando.jpg');
-            this.load.image('bar-NuevoLigoteo','../assets/cardImages/Bar/Ligoteo2.jpg');
-            this.load.image('bar-Baño','../assets/cardImages/Bar/Baños.jpg');
-            this.load.image('bar-Irte','../assets/cardImages/Bar/Saliendo.jpg');
-            this.load.image('bar-Mano','../assets/cardImages/Bar/Mano.jpg');
-            this.load.image('bar-Ahogar','../assets/cardImages/Bar/beber.jpg');
-
+            this.load.image('universidad','../assets/fondos/Estudios/estudios-uni.jpeg');
+            this.load.image('gradosuperior','../assets/fondos/Estudios/estudios-fp.jpeg');
+            this.load.image('chamba','../assets/fondos/Estudios/estudios-chamba.jpg');
+            this.load.image('nini','../assets/fondos/Estudios/estudios-mantenido.jpeg');
+            this.load.image('inventorcaserocrea-inetors','../assets/fondos/Estudios/estudios-creainators.png');
+            this.load.image('buscarconsejo','../assets/fondos/Estudios/estudios-consejo.jpg');
+*/
             //Grado Superior
             this.load.json('dataGSuperior','../data/Dialogue/DialogFP.json');
-            this.load.json('cardGSuperior','../data/Cartas/CartasFP.json');
+            this.load.json('cartasGSuperior','../data/Cartas/CartasFP.json');
             this.load.image('backgroundFP','../assets/fondos/Grado Superior/gs-background.jpg');
 
             this.load.image('gs-PruebaProfesor','../assets/cardImages/Grado Superior/gs-PruebasProfesor.jpeg');
@@ -120,6 +115,33 @@ export default class MenuScene extends Phaser.Scene {
             this.load.image('bg-Emprededor','../assets/fondos/Grado Superior/gs-Empreddedor.jpg');
             this.load.image('bg-CafeAmargo','../assets/fondos/Grado Superior/gs-CaféAmargo.jpg');
             this.load.image('bg-PrestamoEgoista','../assets/fondos/Grado Superior/gs-PrestamonoGeneroso.jpg');
+
+            // Bar
+            this.load.json('dataBar','../data/Dialogue/DialogBar.json');
+            this.load.json('cartasBar','../data/Cartas/CartasBar.json');
+            this.load.image('backgroundBar','../assets/fondos/Bar/BarBG.jpg');
+
+            this.load.image('bar-ayuda','../assets/cardImages/Bar/Llorando.jpg');
+            this.load.image('bar-NuevoLigoteo','../assets/cardImages/Bar/Ligoteo2.jpg');
+            this.load.image('bar-Baño','../assets/cardImages/Bar/Baños.jpg');
+            this.load.image('bar-Irte','../assets/cardImages/Bar/Saliendo.jpg');
+            this.load.image('bar-Mano','../assets/cardImages/Bar/Mano.jpg');
+            this.load.image('bar-Ahogar','../assets/cardImages/Bar/beber.jpg');
+            //Mantenido
+            this.load.json('dataMantenido','../data/Dialogue/DialogMantenido.json');
+            this.load.json('cartasMantenido','../data/Cartas/CartasMantenido.json');
+            this.load.image('backgroundMantenido','../assets/fondos/Mantenido/Mantenido.png');
+ 
+            this.load.image('mantenido-genio','../assets/cardImages/Mantenido/GenioIncomprendido.jpeg');
+            this.load.image('mantenido-fan','../assets/cardImages/Mantenido/fan.jpg');
+            this.load.image('mantenido-otro','../assets/cardImages/Mantenido/OtroDiaMas.jpg');
+            this.load.image('mantenido-apoyo','../assets/cardImages/Mantenido/ApoyarEnCasa.png');
+            this.load.image('mantenido-hacuma','../assets/cardImages/Mantenido/Hacuma Mapapa.jpg');
+            this.load.image('mantenido-puf','../assets/cardImages/Mantenido/puffffffff.jpg');
+
+            
+
+            
 
             //Sprites
             this.load.image('gato', '../assets/sprites/Gato.png');
@@ -152,6 +174,11 @@ export default class MenuScene extends Phaser.Scene {
             this.deck = this.cache.json.get('cartasEstudios');
             this.datos = this.cache.json.get('dataEstudios');
             this.add.image(0,0,'backgroundEstudios').setOrigin(0,0).setScale(1.7,1.7);
+            break;
+        case (5):
+            this.deck = this.cache.json.get('cartasGSuperior');
+            this.datos = this.cache.json.get('dataGSuperior');
+            this.add.image(0, 0, 'backgroundFP').setOrigin(0,0).setScale(1.4,1.4);
             break;
         case(6):
             this.deck = this.cache.json.get('cartasBar');
